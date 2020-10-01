847258c1b6ac939e2c019bebee4684e293bde927
jplozada <juxnpx@gmail.com>
2020-10-01 16:04:04 -0400

    Error component created, validation Formulario form

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Error from "./Error";
 
 const Formulario = () => {
+
+    const [termino, guardarTermino] = useState('');
+    const [error, guardarError] = useState(false);
+
+    const buscarImagenes = e => {
+        e.preventDefault();
+
+        // Validar
+        if(termino.trim() === ''){
+            guardarError(true);
+            return;
+        }
+        guardarError(false);
+        //Enviar el termino de busqueda hacia el componente principal 
+    }
+
     return (  
-        <form>
+        <form
+            onSubmit={buscarImagenes}
+        >
             <div className="row">
                 <div className="form-group col-md-8">
                     <input
                         type="text"
                         className="form-control form-control-lg"
                         placeholder="Busca una imagen, ejemplo: futbol o café"
+                        onChange={e => guardarTermino(e.target.value)}
                     />
                 </div>
                 <div className="form-group col-md-4">
@@ -19,6 +39,8 @@ const Formulario = () => {
                     />
                 </div>
             </div>
+
+            {error ? <Error mensaje="Agrega un término de busqueda" /> : null }
         </form>
     );
 }
