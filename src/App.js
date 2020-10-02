377bcda69ad6a1c49e3d9ed3509d1357cc4a15fb
jplozada <juxnpx@gmail.com>
2020-10-01 23:02:04 -0400

    Fetch data from Pixabay API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Formulario from "./components/Formulario";
 
 function App() {
+
+  // State de la app
+  const [busqueda, guardarBusqueda] = useState('');
+
+  useEffect(() => {
+    const consultarApi = async () => {
+      if(busqueda === '') return;
+
+      const imagenesPorPagina = 30;
+      const key = '18529589-cf58a7c49f0b5dc6662f166f5';
+      const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}`;
+      const respuesta = await fetch(url);
+      const resultado = await respuesta.json();
+      guardarBusqueda(resultado.hits);       
+    }
+    consultarApi();
+  }, [busqueda])
+
   return (
     <div className="container">
       <div className="jumbotron">
         <p className="lead text-center">Buscador de Imágenes</p>
-        <Formulario/>
+        <Formulario
+          guardarBusqueda={guardarBusqueda}
+        />
       </div>
     </div>
   );
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import Error from "./Error";
 
-const Formulario = () => {
+const Formulario = ({guardarBusqueda}) => {
 
     const [termino, guardarTermino] = useState('');
     const [error, guardarError] = useState(false);
@@ -16,6 +16,7 @@ const Formulario = () => {
         }
         guardarError(false);
         //Enviar el termino de busqueda hacia el componente principal 
+        guardarBusqueda(termino);
     }
 
     return (  
