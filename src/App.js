084146bee45737f883c8e7699d0e22a798a01971
jplozada <juxnpx@gmail.com>
2020-10-01 23:16:42 -0400

    Imagen and ListadoImagenes compoenents created to show API data on screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, {useState, useEffect} from 'react';
 import Formulario from "./components/Formulario";
+import ListadoImagenes from './components/ListadoImagenes';
 
 function App() {
 
   // State de la app
   const [busqueda, guardarBusqueda] = useState('');
+  const [imagenes, guardarImagenes] = useState([]);
 
   useEffect(() => {
     const consultarApi = async () => {
@@ -15,7 +17,7 @@ function App() {
       const url = `https://pixabay.com/api/?key=${key}&q=${busqueda}&per_page=${imagenesPorPagina}`;
       const respuesta = await fetch(url);
       const resultado = await respuesta.json();
-      guardarBusqueda(resultado.hits);       
+      guardarImagenes(resultado.hits);       
     }
     consultarApi();
   }, [busqueda])
@@ -26,6 +28,11 @@ function App() {
         <p className="lead text-center">Buscador de Imágenes</p>
         <Formulario
           guardarBusqueda={guardarBusqueda}
+        />
+      </div>
+      <div className="row justify-content-center">
+        <ListadoImagenes
+          imagenes={imagenes}
         />
       </div>
     </div>
